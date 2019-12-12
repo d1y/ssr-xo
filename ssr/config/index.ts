bc3d00d0f684f40d18687fdb7559c4a5b27174fd
d1y <chenhonzhou@gmail.com>
2019-12-13 00:59:41 +0800

    Promise rcv 要返回一个随机端口

--- ssr/config/index.ts
+++ ssr/config/index.ts
@@ -5,5 +5,5 @@ export const devPort: number = 2333
 export const interPort = (port?: number): Promise<number> => new Promise(async rcv=> {
   if (port) return port
   if (isDev) return rcv(devPort)
-  return await getport()
+  return rcv(await getport())
 })
