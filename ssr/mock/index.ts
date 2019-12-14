1e300428a2b6182d4ce00342164371f1f8063d8b
d1y <chenhonzhou@gmail.com>
2019-12-15 01:36:19 +0800

    [fix] 添加 ss 测试链接

--- ssr/mock/index.ts
+++ ssr/mock/index.ts
@@ -6,8 +6,8 @@ export const mockSSR: ssrConfig = {
   port: 543,
   password: '9CU44I',
   cipher: 'chacha20-ietf',
-  shareOverLan: true,
-  listen_port: 1080,
+  // shareOverLan: true,
+  // listen_port: 1080,
   protocol: 'origin',
   obfs: 'http_simple_compatible',
   obfs_param: 'http://e324137811.microsoft.com'
@@ -16,3 +16,6 @@ export const mockSSR: ssrConfig = {
 
 // ssr测试链接
 export const mockSSRLink = `ssr://aGswMy5mdWJ1a2kubGl2ZTo1NTY6YXV0aF9hZXMxMjhfbWQ1OmFlcy0yNTYtY2ZiOmh0dHBfc2ltcGxlOlRHcDBSbnA1Lz9vYmZzcGFyYW09WlRNeU5ERXpOemd4TVM1dGFXTnliM052Wm5RdVkyOXQmcHJvdG9wYXJhbT1NemM0TVRFNlJqQkRZemRKJnJlbWFya3M9VzBKeWIyNTZaVjFiTUM0NFhTRHBwcG5tdUs4d015QXRJRWhMVk9hSmstYWN1dWVsbnVlNnZ5QXRJRk5UVXVXTmotaXVyZyZncm91cD01cXloNVlXRDZaTy01bzZs`
+
+// ss测试链接
+export const mockSSLink = `ss://YWVzLTI1Ni1jZmI6c3N4LnJlLTA2NTkwNzc2QDE5Mi4yNDEuMjE1LjE5MToxNjExMA==`
